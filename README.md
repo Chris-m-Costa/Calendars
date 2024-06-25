0dbffc4fda81f796816513e5db141d20c4d81d55
C. Costa <104877987+rhubarb-watermelon@users.noreply.github.com>
2024-06-25 14:57:00 -0400

    Cleanup Files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Calendars
- Sport Calendars
--- newtest.py
+++
@@ -1,8 +0,0 @@
-url = 'https://www.nuerburgring-langstrecken-serie.de/en/calendar-nurburgring-langstrecken-serie-2023/'
-
-import Scraper_Tools
-import Calendar_tools
-
-nls_soup = make_soup(url)
-
-scrape_nls(nls_soup)
